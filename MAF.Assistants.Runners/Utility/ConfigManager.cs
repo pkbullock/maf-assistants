ea4f707f4e4ef067fda378c2f1d678754b066d95
Paul Bullock <me@pkbullock.com>
2025-10-24 16:56:44 +0100

    Added Config Manager

---
+++ MAF.Assistants.Runners/Utility/ConfigManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MAF.Assistants.Utility
+{
+    public class ConfigManager
+    {
+        public record Configuration(
+            string AzureOpenAiEndpoint,
+            string AzureOpenAiKey,
+            string AzureChatDeploymentName,
+            string AzureEmbeddingModelName,
+            
+            string FoundryLocalChatDeploymentName,
+            string OllamaLocalEmbeddingModelName
+         );
+
+        public static Configuration GetConfig()
+        {
+            IConfigurationRoot configurationRoot = new ConfigurationBuilder().AddUserSecrets<ConfigManager>().Build();
+            
+            string azureOpenAiEndpoint = configurationRoot["AzureOpenAiEndpoint"] ?? string.Empty;
+            string azureOpenAiKey = configurationRoot["AzureOpenAiKey"] ?? string.Empty;
+            string azureChatDeploymentName = configurationRoot["AzureChatDeploymentName"] ?? string.Empty;
+            string azureEmbeddingModelName = configurationRoot["AzureEmbeddingModelName"] ?? string.Empty;
+
+            string foundryLocalChatDeploymentName = configurationRoot["FoundryLocalChatDeploymentName"] ?? string.Empty;
+            string ollamaLocalEmbeddingModelName = configurationRoot["OllamaLocalEmbeddingModelName"] ?? string.Empty;
+
+            return new Configuration(
+                azureOpenAiEndpoint,
+                azureOpenAiKey,
+                azureChatDeploymentName,
+                azureEmbeddingModelName,
+
+                foundryLocalChatDeploymentName,
+                ollamaLocalEmbeddingModelName);
+        }
+    }
+}
